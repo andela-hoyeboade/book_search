cfe95863639b15e62f8a8892c710b62335f8b23e
Hassan Oyeboade <hassan.oyeboade@andela.com>
2016-10-24 14:43:37 +0100

    [Feature] Write test for models

---
+++ book_search/books/tests/test_models.py
@@ -0,0 +1,22 @@
+from books.models import Book, Category
+from django.test import TestCase
+
+
+class TestCategory(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(name='Health')
+
+    def test_category_object_representation(self):
+        self.assertEqual(str(self.category), self.category.name)
+
+
+class TestBook(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(name='Health')
+        self.book = Book.objects.create(
+            title='Living a healthy lifestyle', category=self.category)
+
+    def test_book_object_representation(self):
+        self.assertEqual(str(self.book), self.book.title)

08121b968f861a8d7d23493af47e1e7baa684940
Hassan Oyeboade <hassan.oyeboade@andela.com>
2016-10-24 14:47:55 +0100

    [Feature] Create all book related models

--- book_search/books/models.py
+++ book_search/books/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name='books')
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    def __unicode__(self):
+        return self.title
